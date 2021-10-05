e8f51ee8c148c69b278253e98bbfd99117f64141
BrynDows <bryan_tiban_tigse@hotmail.com>
2021-10-05 18:34:07 +0200

    Initial commit

---
+++ HiringAssistant.java
@@ -0,0 +1,10 @@
+package kataCombinaciones;
+
+import java.util.List;
+
+public interface HiringAssistant {
+
+	List<String> searchMinimalAmount();
+	
+	void add(Service service);
+}
---
+++ Service.java
@@ -0,0 +1,5 @@
+package kataCombinaciones;
+
+public record Service (int id, String name, double amount){
+ 
+}
